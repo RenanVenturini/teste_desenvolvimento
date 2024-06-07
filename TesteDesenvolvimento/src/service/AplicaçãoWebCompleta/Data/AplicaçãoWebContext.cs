ef9031e5a41c61c80d3297a12f21928913e847e0
renan <renanventurinioficial@gmail.com>
2024-06-07 07:33:17 -0300

    Criando Entidade e Classe de contexto EntityFramework

---
+++ TesteDesenvolvimento/src/service/AplicaçãoWebCompleta/Data/AplicaçãoWebContext.cs
@@ -0,0 +1,24 @@
+﻿using AplicaçãoWebCompleta.Data.Table;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplicaçãoWebCompleta.Data
+{
+    public class AplicaçãoWebContext : DbContext
+    {
+        public AplicaçãoWebContext(DbContextOptions<AplicaçãoWebContext> options) : base(options) { }
+
+        public DbSet<CadastroUsuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CadastroUsuario>(entity =>
+            {
+                entity.ToTable("TbCadastroUsuario");
+
+                entity.HasKey(x => x.Id);
+            });
+        }
+    }
+}
